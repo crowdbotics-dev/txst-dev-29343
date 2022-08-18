1d2897f86469c41f67ebc6f68affac638b25344f
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-08-18 12:01:15 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,7 +1,3 @@
-import { CheckBox } from "react-native-elements";
-import { Slider } from "react-native-elements";
-import { Switch } from "react-native";
-import { TextInput } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
@@ -12,37 +8,7 @@ const Untitled1 = () => {
     position: 'relative',
     height: '100%'
   }}>
-      <TextInput style={{
-      left: 12,
-      top: 121,
-      position: "absolute",
-      backgroundColor: "#ffffff",
-      borderColor: "#cccccc",
-      width: 262,
-      height: 30
-    }}></TextInput><Switch style={{
-      left: 140,
-      top: 271,
-      position: "absolute",
-      width: 50,
-      height: 25,
-      transform: "rotate(90deg)"
-    }} thumbColor="#c42121"></Switch><Slider thumbStyle={{
-      height: 20,
-      width: 20
-    }} thumbTintColor="#0000FF" maximumValue={11} minimumValue={11} style={{
-      position: "absolute",
-      left: 53,
-      top: 316,
-      width: 257,
-      height: 80
-    }} minimumTrackTintColor="#9f3d3d" maximumTrackTintColor="#66bd41" value={22} step={24}></Slider><CheckBox title="Checkbox Title" style={{
-      left: 121,
-      top: 454,
-      position: "absolute",
-      width: 236,
-      height: 80
-    }}></CheckBox></View>;
+      </View>;
 };
 
 export default Untitled1;
